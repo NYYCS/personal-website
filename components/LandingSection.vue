53895c61ec4c9fefc7fe2a1419e0e144475fce58
Ng Yu Yue <nyystu@gmail.com>
2023-02-28 19:55:03 +0800

    style: fix css for light mode users

---
+++ components/LandingSection.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <span class="name">NG YU YUE</span>
+    <p>Hey there friend.</p>
+    <p>
+      I am currently a 2nd year computer science student studying in Fudan
+      University located in Shanghai.
+    </p>
+    <p>
+      Feel free to contact me for freelance opportunities on
+      <a class="underline" href="mailto:hello@nyydev.me">hello@nyydev.me</a>
+    </p>
+    <ul class="socials">
+      <li>Find me on:</li>
+      <li><a href="https://github.com/NYYCS">GitHub</a></li>
+      <li><a href="https://linkedin.com/in/nyydev">LinkedIn</a></li>
+    </ul>
+  </section>
+</template>
+
+<style scoped>
+.name {
+  font-size: 0.8em;
+  font-weight: 900;
+  letter-spacing: 0.3ch;
+}
+
+.socials {
+  display: flex;
+  list-style: none;
+  padding: 0;
+  gap: 0.5em;
+  margin-top: 2em;
+}
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,16 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from "./components/HelloWorld.vue";
+import LandingSection from "../components/LandingSection.vue";
+import ProjectsSection from "../components/ProjectsSection.vue";
 </script>
 
 <template>
   <main>
-    <section>
-      <span class="name">NG YU YUE</span>
-      <p>Hey there friend.</p>
-      <p>
-        I am currently a 2nd year computer science student studying in Fudan
-        University located in Shanghai.
-      </p>
-      <p>
-        Feel free to contact me for freelance opportunities on
-        <a class="email" href="mailto:hello@nyydev.me">hello@nyydev.me</a>
-      </p>
-      <ul class="socials">
-        <li>Find me on:</li>
-        <li><a href="https://github.com/NYYCS">GitHub</a></li>
-        <li><a href="https://linkedin.com/in/nyydev">LinkedIn</a></li>
-      </ul>
-    </section>
+    <LandingSection />
   </main>
 </template>
 
 <style scoped>
-.name {
-  color: white;
-  font-size: 0.8em;
-  font-weight: 900;
-  letter-spacing: 0.3ch;
-}
-
-a {
-  position: relative;
-  display: inline-block;
-  color: white;
-  opacity: 0.5;
-  font-weight: initial;
-  transition: all ease-in-out 0.4s;
-}
-
-a.email::after {
-  content: "";
-  display: inline-block;
-  position: absolute;
-  width: 100%;
-  height: 1px;
-  background: white;
-  left: 0;
-  bottom: -4px;
-}
-
-a:hover {
-  transform: scale(1.01);
-  opacity: 1;
-}
-
-.socials {
-  display: flex;
-  list-style: none;
-  padding: 0;
-  gap: 0.5em;
-  margin-top: 2em;
+section {
+  padding: 2rem 0;
 }
 </style>
